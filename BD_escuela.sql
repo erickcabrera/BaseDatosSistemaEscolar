fc7c884d5cf3f7adc44dd3277a73c49ad72664e3
fabs1902 <fabiolamartinez190201@gmail.com>
2020-10-31 23:22:04 -0600

    Update BD_escuela.sql
    Cambios en procedimiento
    ps_buscar_grados_curso

--- BD_escuela.sql
+++ BD_escuela.sql
@@ -895,3 +895,27 @@ rollback
 print error_message()
 end catch;
 GO
+
+--Mostrar Grados
+create proc ps_buscar_grados_curso
+@nombreGrado varchar(20),
+@idprofesor int
+as
+begin try
+begin tran
+select m.nombreMateria as [Materia],g.nombreGrado as [Grado], s.Seccion as [Sección], dt.anioEscolar as [Año Escolar], COUNT(Ra.id_Alumno) as [Cantidad de alumnos]
+from Curso as C 
+INNER JOIN Materia as M on C.id_Materia=M.id_Materia 
+INNER JOIN Profesor as P on P.id_Profesor=C.id_Profesor
+INNER JOIN Detalle_Grado_Seccion  as DT on c.id_Detalle_Grado_Seccion=DT.id_Detalle_Grado_Seccion
+INNER JOIN Seccion as S on DT.id_Seccion=s.id_Seccion
+INNER JOIN Grado as G on dt.id_Grado=g.id_Grado
+INNER JOIN Registro_Alumno as Ra on dt.id_Detalle_Grado_Seccion=ra.id_Alumno
+WHERE p.id_Profesor=@idprofesor and nombreGrado LIKE ('%'+@nombreGrado+'%')  Group by m.nombreMateria,g.nombreGrado,s.Seccion,dt.anioEscolar 
+commit
+end try
+begin catch
+rollback
+print error_message()
+end catch;
+GO
